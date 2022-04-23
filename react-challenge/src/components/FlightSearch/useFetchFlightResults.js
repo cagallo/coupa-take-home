305c9b87943c069b6c887bd9e38388927de53ca0
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-04-23 00:29:04 -0600

    Remove duplicate flights

--- react-challenge/src/components/FlightSearch/useFetchFlightResults.js
+++ react-challenge/src/components/FlightSearch/useFetchFlightResults.js
@@ -14,8 +14,21 @@ const useFetchFlightResults = () => {
        * Results come back as an array of [outbound, return] flights and
        * we just need the outbound flights for the purpose of this challenge
        */
-      const outboundSegmentFlights = flights.map(flight => flight[0]);
 
+      let dupChecker = {}; // track encountered flight numbers
+      const outboundSegmentFlights = flights.reduce((acc, flight) => {
+        flight = flight[0];
+        let key = flight.segmentsArray[0].flights[0].flightNumber;
+        
+        // only show flights of the same flight number once
+        if (!dupChecker[key]) {
+          acc.push(flight);
+          dupChecker[key] = key;
+        }
+        return acc;
+     }, []);
+
+      console.log(outboundSegmentFlights)
       setFlights(outboundSegmentFlights);
     } catch (error) {
       setError(error);
