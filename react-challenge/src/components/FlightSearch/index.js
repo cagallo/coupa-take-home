b72a1b37e5aca81fa6772350b7af465f1d048043
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-04-23 00:30:27 -0600

    Add sort conditionals and click event functions

--- react-challenge/src/components/FlightSearch/index.js
+++ react-challenge/src/components/FlightSearch/index.js
@@ -1,19 +1,60 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './index.style.css';
 import FlightSearchItem from './FlightSearchItem';
 import FlightSearchFooter from './FlightSearchFooter';
 import useFetchFlightResults from './useFetchFlightResults';
 import SortBy from './filters/SortBy';
-import { SortByDefaultOption } from './filters/SortBy/enums';
+import { SortByDefaultOption, SortByEnum } from './filters/SortBy/enums';
+import moment from 'moment';
 
 export default function FlightSearch() {
+  const flightsPerPage = 4;
+
   const [sortBy, setSortBy] = useState(SortByDefaultOption);
+  const [currentPage, setCurrentPage] = useState(1)
+  //const [paginatedFlights, setPaginatedFlights ] = useState([]);
+  const [offset, setOffset] = useState(flightsPerPage)
 
   // Fetch Flights
-  const { flights } = useFetchFlightResults();
+  let { flights } = useFetchFlightResults();
+  if (sortBy.value === SortByEnum.BEST) {
+    flights = flights.sort((a, b) => a.score - b.score)
+  } else if (sortBy.value === SortByEnum.PRICE_LOW) {
+    flights = flights.sort((a, b) => a.price - b.price)
+  } else if (sortBy.value === SortByEnum.TIME_OF_DAY) {
+    // sort the data by date using moment.js
+    flights = flights.sort((a, b) => {
 
-  // Only show 10 flight results per page
-  const paginatedFlights = flights.slice(0, 8);
+      // get date/time values from segmentsArray
+      let currFlightDeparture = a.segmentsArray[0].departsAt;
+      let nextFlightDeparture = b.segmentsArray[0].departsAt;;
+      
+      // get difference in milliseconds between the two times
+      return moment.utc(currFlightDeparture).diff(moment.utc(nextFlightDeparture))
+    });
+  }
+
+  const maxPage = Math.ceil(flights.length / flightsPerPage)
+
+  const handlePreviousPage = (e) => {
+    if (currentPage <= 1) {
+      return;
+    }
+    let prevPage = currentPage - 1
+    setCurrentPage(prevPage)
+    setOffset(offset - flightsPerPage)
+  }
+
+  const handleNextPage = (e) => {
+    if (currentPage >= maxPage) {
+      return;
+    }
+    let nextPage = currentPage + 1
+    setCurrentPage(nextPage)
+    setOffset(offset + flightsPerPage)
+  }
+
+  const paginatedFlights = flights.slice(offset - flightsPerPage, offset);
 
   return (
     <div className="row">
@@ -34,7 +75,7 @@ export default function FlightSearch() {
         </div>
       </div>
       {/* Pagination */}
-      <FlightSearchFooter />
+      <FlightSearchFooter handleNextPage={handleNextPage} handlePreviousPage={handlePreviousPage} currentPage={currentPage} maxPage={maxPage}/>
     </div>
   );
 }
