c5274bc99e02431f8e1e99dc89f48c529c5a7d24
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-04-23 00:29:41 -0600

    Add functions to handle click events for pagination

--- react-challenge/src/components/FlightSearch/FlightSearchFooter.js
+++ react-challenge/src/components/FlightSearch/FlightSearchFooter.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Button from '../Button';
 
-export default function FlightSearchPagination() {
+export default function FlightSearchPagination({ handleNextPage, handlePreviousPage, currentPage,  maxPage }) {
+
   return (
     <div className="m-t-2 m-b-2 d-space-between full-width">
-      <Button size="sm">Previous Page</Button>
-      <p className="m-t-0 m-b-0">Page 0 of 0</p>
-      <Button size="sm">Next Page</Button>
+      <Button disabled size="sm" onClick={handlePreviousPage}>Previous Page</Button>
+      <p className="m-t-0 m-b-0">Page {currentPage} of {maxPage}</p>
+      <Button size="sm" onClick={handleNextPage}>Next Page</Button>
     </div>
   );
+  
 }
+
+
+
+
