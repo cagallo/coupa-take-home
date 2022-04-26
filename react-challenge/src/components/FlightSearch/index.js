1ffb805af1da3a32f97c7087792456629441f84f
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-04-26 10:48:05 -0700

    Make sorting function and add error handling

--- react-challenge/src/components/FlightSearch/index.js
+++ react-challenge/src/components/FlightSearch/index.js
@@ -5,49 +5,55 @@ import FlightSearchFooter from './FlightSearchFooter';
 import useFetchFlightResults from './useFetchFlightResults';
 import SortBy from './filters/SortBy';
 import { SortByDefaultOption, SortByEnum } from './filters/SortBy/enums';
-import moment from 'moment';
+import { sortFuncs } from './sortFunctions';
+import { filterByTime } from './utils';
+import FilterBy from './filters/FilterBy';
 
 export default function FlightSearch() {
   const flightsPerPage = 4;
-
   const [sortBy, setSortBy] = useState(SortByDefaultOption);
+  const [filterBy, setFilterBy] = useState(null)
   const [currentPage, setCurrentPage] = useState(1)
-  //const [paginatedFlights, setPaginatedFlights ] = useState([]);
   const [offset, setOffset] = useState(flightsPerPage)
 
-  // Fetch Flights
-  let { flights } = useFetchFlightResults();
-  if (sortBy.value === SortByEnum.BEST) {
-    flights = flights.sort((a, b) => a.score - b.score)
-  } else if (sortBy.value === SortByEnum.PRICE_LOW) {
-    flights = flights.sort((a, b) => a.price - b.price)
-  } else if (sortBy.value === SortByEnum.TIME_OF_DAY) {
-    // sort the data by date using moment.js
-    flights = flights.sort((a, b) => {
 
-      // get date/time values from segmentsArray
-      let currFlightDeparture = a.segmentsArray[0].departsAt;
-      let nextFlightDeparture = b.segmentsArray[0].departsAt;;
-      
-      // get difference in milliseconds between the two times
-      return moment.utc(currFlightDeparture).diff(moment.utc(nextFlightDeparture))
-    });
-  }
+  let errorMsg;
+
+  // Fetch Flights
+
+  let { flights } = useFetchFlightResults()
+
+  const sortFlights = () => {
+    console.log('here')
+    if (!flights.length) {
+      errorMsg = `We are sorry, there are no available flights at this time, please try again later`
+      return errorMsg
+    }
+      if (sortBy.value === SortByEnum.BEST) {
+        return sortFuncs.sortByBest(flights)
+      } else if (sortBy.value === SortByEnum.PRICE_LOW) {
+        return sortFuncs.sortByPrice(flights)
+      } else if (sortBy.value === SortByEnum.TIME_OF_DAY) {
+        return sortFuncs.sortByTime(flights)
+      }
+    }
+
+  sortFlights()
 
   const maxPage = Math.ceil(flights.length / flightsPerPage)
 
-  const handlePreviousPage = (e) => {
+  const handlePreviousPage = () => {
     if (currentPage <= 1) {
-      return;
+      return currentPage;
     }
     let prevPage = currentPage - 1
     setCurrentPage(prevPage)
     setOffset(offset - flightsPerPage)
   }
 
-  const handleNextPage = (e) => {
+  const handleNextPage = () => {
     if (currentPage >= maxPage) {
-      return;
+      return maxPage;
     }
     let nextPage = currentPage + 1
     setCurrentPage(nextPage)
@@ -64,10 +70,12 @@ export default function FlightSearch() {
             <b>Select an outbound flight</b>
             <p className="m-v-0 fade small">DEN → CHI</p>
           </div>
+          <FilterBy value={filterBy} onChange={setFilterBy}/>
           <SortBy value={sortBy} onChange={setSortBy} />
         </div>
         {/* Display Flight Results */}
-        <div className="pane-content">
+        <div className="pane-content" data-testid='pane-content'>
+          {!flights.length && errorMsg}
           {Array.isArray(paginatedFlights) &&
             paginatedFlights.map(flight => (
               <FlightSearchItem key={flight.id} flight={flight} />
@@ -79,3 +87,8 @@ export default function FlightSearch() {
     </div>
   );
 }
+
+// module.exports = {
+//   handlePreviousPage,
+//   handleNextPage
+// }
