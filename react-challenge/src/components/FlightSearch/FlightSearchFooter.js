1e03e260ce57b200efc99d67097ae0fe1bf4bf8e
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-04-26 10:46:36 -0700

    Add pagination

--- react-challenge/src/components/FlightSearch/FlightSearchFooter.js
+++ react-challenge/src/components/FlightSearch/FlightSearchFooter.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Button from '../Button';
 
-export default function FlightSearchPagination({ handleNextPage, handlePreviousPage, currentPage,  maxPage }) {
+export default function FlightSearchPagination({ handleNextPage, handlePreviousPage, currentPage, maxPage }) {
 
   return (
     <div className="m-t-2 m-b-2 d-space-between full-width">
-      <Button disabled size="sm" onClick={handlePreviousPage}>Previous Page</Button>
-      <p className="m-t-0 m-b-0">Page {currentPage} of {maxPage}</p>
-      <Button size="sm" onClick={handleNextPage}>Next Page</Button>
+      <Button data-testid='prev-button' size="sm" onClick={handlePreviousPage}>Previous Page</Button>
+      <p className="m-t-0 m-b-0" data-testid='page-details'>Page {currentPage} of {maxPage}</p>
+      <Button data-testid='next-button'size="sm" onClick={handleNextPage}>Next Page</Button>
     </div>
   );
   
